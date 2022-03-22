960fc5d91de5f7bbd75587fee9bb3ea148d12579
hien.th <trinhhien301@gmail.com>
2022-03-22 10:28:59 +0700

    abstract

--- App/Interfaces/index.php
+++ App/Interfaces/index.php
@@ -1,11 +1,54 @@
 <?php
-// class SimpleBook có phương thức getAuthor() và getTitle()
-class SimpleBook {
+
+/**
+ * Abstract Factory Pattern Cung cấp một interface cho việc khởi tạo các tập hợp của những object có đặc điểm giống nhau
+ * mà không cần quan tâm object đó là gì.
+ */
+
+// abstract class với các phương thức tạo ra sản phẩm được định nghĩa
+abstract class AbstractBookFactory {
+    abstract function makePHPBook();
+    abstract function makeMySQLBook();
+}
+
+// khai báo các nền tảng
+class OReillyBookFactory extends AbstractBookFactory {
+    private $context = "OReilly";
+    function makePHPBook() {
+        return new OReillyPHPBook;
+    }
+    function makeMySQLBook() {
+        return new OReillyMySQLBook;
+    }
+}
+
+class SamsBookFactory extends AbstractBookFactory {
+    private $context = "Sams";
+    function makePHPBook() {
+        return new SamsPHPBook;
+    }
+    function makeMySQLBook() {
+        return new SamsMySQLBook;
+    }
+}
+
+// abstract class định nghĩa các function
+abstract class AbstractBook {
+    abstract function getAuthor();
+    abstract function getTitle();
+}
+
+abstract class AbstractMySQLBook extends AbstractBook {
+    protected $subject = "MySQL";
+}
+
+// các class extend sẽ overwrite các function được định nghĩa trong abstract class nhằm phục vụ mục đích của class
+class OReillyMySQLBook extends AbstractMySQLBook {
     private $author;
     private $title;
-    function __construct($author_in, $title_in) {
-        $this->author = $author_in;
-        $this->title  = $title_in;
+    function __construct() {
+        $this->author = 'George Reese, Randy Jay Yarger, and Tim King';
+        $this->title = 'Managing and Using MySQL';
     }
     function getAuthor() {
         return $this->author;
@@ -15,21 +58,103 @@ class SimpleBook {
     }
 }
 
-// class BookAdapter là lớp chuyển đổi của class SimpleBook nhằm mục đích tạo phương thức getAuthorAndTitle() mà không phải
-// sửa đổi lớp gốc (SimpleBook).
-class BookAdapter {
-    private $book;
-    function __construct(SimpleBook $book_in) {
-        $this->book = $book_in;
+class SamsMySQLBook extends AbstractMySQLBook {
+    private $author;
+    private $title;
+    function __construct() {
+        $this->author = 'Paul Dubois';
+        $this->title = 'MySQL, 3rd Edition';
     }
-    function getAuthorAndTitle() {
-        return $this->book->getTitle().' by '.$this->book->getAuthor();
+    function getAuthor() {
+        return $this->author;
+    }
+    function getTitle() {
+        return $this->title;
     }
 }
 
-// TESTING ADAPTER PATTERN
-$book = new SimpleBook("Gamma, Helm, Johnson, and Vlissides", "Design Patterns");
-$bookAdapter = new BookAdapter($book);
-writeln('Author and Title: '.$bookAdapter->getAuthorAndTitle());
+abstract class AbstractPHPBook extends AbstractBook {
+    protected $subject = "PHP";
+}
+
+// các class extend sẽ overwrite các function được định nghĩa trong abstract class nhằm phục vụ mục đích của class
+class OReillyPHPBook extends AbstractPHPBook {
+    private $author;
+    private $title;
+    private static $oddOrEven = 'odd';
+    function __construct()
+    {
+        //alternate between 2 books
+        if ('odd' == self::$oddOrEven) {
+            $this->author = 'Rasmus Lerdorf and Kevin Tatroe';
+            $this->title = 'Programming PHP';
+            self::$oddOrEven = 'even';
+        }
+        else {
+            $this->author = 'David Sklar and Adam Trachtenberg';
+            $this->title = 'PHP Cookbook';
+            self::$oddOrEven = 'odd';
+        }
+    }
+    function getAuthor() {
+        return $this->author;
+    }
+    function getTitle() {
+        return $this->title;
+    }
+}
+
+class SamsPHPBook extends AbstractPHPBook {
+    private $author;
+    private $title;
+    function __construct() {
+        //alternate randomly between 2 books
+        mt_srand((double)microtime() * 10000000);
+        $rand_num = mt_rand(0, 1);
+
+        if (1 > $rand_num) {
+            $this->author = 'George Schlossnagle';
+            $this->title = 'Advanced PHP Programming';
+        }
+        else {
+            $this->author = 'Christian Wenz';
+            $this->title = 'PHP Phrasebook';
+        }
+    }
+    function getAuthor() {
+        return $this->author;
+    }
+    function getTitle() {
+        return $this->title;
+    }
+}
+
+// TESTING ABSTRACT FACTORY PATTERN
+writeln('OReillyBookFactory');
+$bookFactoryInstance = new OReillyBookFactory;
+testConcreteFactory($bookFactoryInstance);
+
+writeln('SamsBookFactory');
+$bookFactoryInstance = new SamsBookFactory;
+testConcreteFactory($bookFactoryInstance);
+
+function testConcreteFactory($bookFactoryInstance)
+{
+    $phpBookOne = $bookFactoryInstance->makePHPBook();
+    writeln('first php Author: '.$phpBookOne->getAuthor());
+    writeln('first php Title: '.$phpBookOne->getTitle());
+
+    $phpBookTwo = $bookFactoryInstance->makePHPBook();
+    writeln('second php Author: '.$phpBookTwo->getAuthor());
+    writeln('second php Title: '.$phpBookTwo->getTitle());
+
+    $mySqlBook = $bookFactoryInstance->makeMySQLBook();
+    writeln('MySQL Author: '.$mySqlBook->getAuthor());
+    writeln('MySQL Title: '.$mySqlBook->getTitle());
+}
+
+function writeln($line_in) {
+    echo $line_in."<br/>";
+}
 
 ?>
